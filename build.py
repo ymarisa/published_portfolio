92ddccf264c7049d28bac3327c783606e73da96b
Marisa Yeung <neurosa@gmail.com>
2019-06-21 23:39:44 -0700

    fixed active navbar template, updated build fn

--- build.py
+++ build.py
@@ -3,71 +3,108 @@ import re
 # combine templates with content
 
 TEMPLATE_PATTERN = re.compile(r"{{|}}")
-TEMPLATE_CURRENT_DICT = {
-    "index.html": "index_current_span",
-    "about.html": "about_current_span",
-    "contact.html": "contact_current_span"
-}
-TEMPLATE_TITLE_DICT = {
-    "index.html": "Home",
-    "about.html": "About",
-    "contact.html": "Contact"
-}
+
 CURRENT_SPAN = "<span class=\"sr-only\">(current)</span>"
 
 HOMEDIR = "/Users/marisayeung/Dropbox/ksc/homework/hw_2"
 REPODIR = "ymarisa.github.io"
 CONTENTDIR = "content"
 BUILDDIR = "docs"
-TEMPLATEDIR = "templates"
 FULLCONTENTDIR = HOMEDIR + "/" + REPODIR + "/" + CONTENTDIR
 FULLBUILDDIR = HOMEDIR + "/" + REPODIR + "/" + BUILDDIR
+
+TEMPLATE = "templates/template.html"
 
 PAGES = [
     {
         "filename": FULLCONTENTDIR + "/index.html",
         "output": FULLBUILDDIR + "/index.html",
         "title": "Home",
+        "current": "index_current_span",
     },
     {
         "filename": FULLCONTENTDIR + "/about.html",
         "output": FULLBUILDDIR + "/about.html",
         "title": "About",
+        "current": "about_current_span",
     },
     {
         "filename": FULLCONTENTDIR + "/contact.html",
         "output": FULLBUILDDIR + "/contact.html",
         "title": "Contact",
+        "current": "contact_current_span",
     }
 ]
 
+CURRENT_SPAN_WORDS = [
+    ["{{index_current_span}}", "{{index_active}}"], 
+    ["{{about_current_span}}", "{{about_active}}"], 
+    ["{{contact_current_span}}", "{{contact_active}}"]
+]
+
+# def replace_template_words(page, line):
+#     line_check = TEMPLATE_PATTERN.split(line)
+
+#     if len(line_check) < 2:
+#         return line
+#     if line_check[1] == page["current"]:
+#         line_check[1] = CURRENT_SPAN
+#     elif line_check[1] == "title":
+#         line_check[1] = page["title"]
+#     else:
+#         # don't add a current span if the template word doesn't match the page
+#         line_check[1] = ""
+
+#     return''.join(line_check)
+
 # Search out {{foo}} style template words in the template file and replace
-def replace_template_words(page, line):
-    line_check = TEMPLATE_PATTERN.split(line)
-    if len(line_check) < 2:
-        return line
-    if line_check[1] == TEMPLATE_CURRENT_DICT[page]:
-        line_check[1] = CURRENT_SPAN
-    if line_check[1] == "title":
-        line_check[1] = TEMPLATE_TITLE_DICT[page]
-    return ''.join(line_check)
+# def replace_template_words_foo(page, line):
+#     fname = page["filename"].split
+#     line_check = TEMPLATE_PATTERN.split(line)
+#     if len(line_check) < 2:
+#         return line
+#     if line_check[1] == TEMPLATE_CURRENT_DICT[page]:
+#         line_check[1] = CURRENT_SPAN
+#     if line_check[1] == "title":
+#         line_check[1] = page["title"]
+#     return ''.join(line_check)
+
+def build_page(page):
+    # read in template and content
+    built_page = open(TEMPLATE).read()
+    content = open(page["filename"]).read()
+
+    # replace template words of format {{foo}}
+    built_page = built_page.replace("{{title}}", page["title"])
+    built_page = built_page.replace("{{content}}", content)
+
+    for current in CURRENT_SPAN_WORDS:
+        if current[0] == "{{" + page["current"] + "}}":
+            built_page = built_page.replace(current[0], CURRENT_SPAN)
+            built_page = built_page.replace(current[1], "active")
+        else:
+            built_page = built_page.replace(current[0], "")
+            built_page = built_page.replace(current[1], "")
+
+
+    # write built file
+    open(page["output"], 'w+').write(built_page)
 
 # Generate an html file consisting of top template > content > bottom template
-def build_page(page):
-    title = page["title"]
-    
-    fulltemplatedir = HOMEDIR + "/" + REPODIR + "/" + TEMPLATEDIR
-    top = fulltemplatedir + "/top.html"
-    bottom = fulltemplatedir + "/bottom.html"
+# def build_page(page):
 
-    files = [top, page["filename"], bottom]
-
-    with open(page["output"], 'w') as outfile:
-        for f in files:
-            with open(f) as infile:
-                for line in infile:
-                    line = replace_template_words(page, line)
-                    outfile.write(line)
+#     with open(page["output"], 'w') as outfile:
+#         with open(TEMPLATE) as infile:
+#             for line in infile:
+#                 if line == """{{content}}""":
+#                     print(line)
+#                     #add content file to output
+#                     with open(page["filename"], 'w') as contentfile:
+#                         for content_line in contentfile:
+#                             outfile.write(content_line)
+#                 else:
+#                     line = replace_template_words(page, line)
+#                     outfile.write(line)
 
 def main():
     for page in PAGES:
