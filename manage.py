daedcf4b5865286c2e0f873b2d2382db4de2d418
Marisa Yeung <neurosa@gmail.com>
2019-07-03 02:54:16 -0700

    added command line options

--- manage.py
+++ manage.py
@@ -1,17 +1,27 @@
 import utils
+import sys
 
 
-def main():
+def main(argv):
+    if len(argv) < 1:
+        print("Usage:\n" +
+              "     Rebuild site:     python manage.py build\n" +
+              "     Create new page:  python manage.py new"
+              )
+        exit(0)
+    option = argv[0]
+
     build_dir = "docs"
     content_dir = "content/*.html"
 
     template = "templates/template.html"
 
-    pages = utils.get_content_pages(content_dir, build_dir)
+    if option == "build":
+        pages = utils.get_content_pages(content_dir, build_dir)
 
-    for page in pages:
-        utils.build_page(page, pages, template)
+        for page in pages:
+            utils.build_page(page, pages, template)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
